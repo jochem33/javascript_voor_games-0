c29c54ce64285104953982b080b3b1bab4011897
jochem33 <jochemjuriaan.baas@coderclass.nl>
2017-10-09 18:55:20 +0200

    [add] tekening afgemaakt

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 //variabelen maken:
 var autowidth = 100;
-var autoheight = 40;
-var autoX = 30;
-var autoY = 30;
+var autoheight = 20;
+var autoX = 100;
+var autoY = 140;
+var wielsize = 20;
 
 function setup() {
   createCanvas(300, 300);
@@ -11,6 +12,13 @@ function setup() {
 
 function draw() {
   noStroke();
-  fill(69, 193, 90)
-  rect()
+  fill(69, 193, 90);
+  rect(autoX, autoY, autowidth, autoheight);
+  rect(autoX + 10, autoY - 20, autowidth - 20, autoheight);
+  stroke(0);
+  strokeWeight(2);
+  fill(0, 255, 216);
+  ellipse(120, 160, wielsize, wielsize);
+  ellipse(180, 160, wielsize, wielsize);
+  line(0, 170, 300, 170);
 }
