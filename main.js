360561d439f4b09755bafc64e061bb6d3c26cf58
jochem33 <jochemjuriaan.baas@coderclass.nl>
2017-10-09 19:02:35 +0200

    [add] auto rijd nu! [change] auto in apparte functie

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var autoheight = 20;
 var autoX = 100;
 var autoY = 140;
 var wielsize = 20;
+var autospeed = 1;
 
 function setup() {
   createCanvas(300, 300);
@@ -11,14 +12,23 @@ function setup() {
 }
 
 function draw() {
+  background(220);
   noStroke();
+  auto()
+  autoX = autoX + autospeed;
+  // lijn
+  line(0, 170, 300, 170);
+}
+
+
+function auto() {
   fill(69, 193, 90);
   rect(autoX, autoY, autowidth, autoheight);
   rect(autoX + 10, autoY - 20, autowidth - 20, autoheight);
   stroke(0);
   strokeWeight(2);
   fill(0, 255, 216);
-  ellipse(120, 160, wielsize, wielsize);
-  ellipse(180, 160, wielsize, wielsize);
+  ellipse(autoX + 20, 160, wielsize, wielsize);
+  ellipse(autoX + 80, 160, wielsize, wielsize);
   line(0, 170, 300, 170);
 }
