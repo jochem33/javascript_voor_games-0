568c43432b64d2d07ac78ac7de5b65e71dec03f0
jochem33 <jochemjuriaan.baas@coderclass.nl>
2017-10-09 10:30:12 +0200

    [add] draw functie toegevoegd

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
+//variabelen maken:
+var autowidth = 100;
+var autoheight = 40;
+var autoX = 30;
+var autoY = 30;
+
 function setup() {
-  createCanvas(300, 300)
-  background(220)
+  createCanvas(300, 300);
+  background(220);
+}
+
+function draw() {
+  noStroke();
+  fill(69, 193, 90)
+  rect()
 }
