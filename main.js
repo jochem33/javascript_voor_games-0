8ea9fe780886a03024f48b5e45a4c0f25d6058f5
jochem33 <jochemjuriaan.baas@coderclass.nl>
2017-10-10 10:23:57 +0200

    [change] bal bug gefixt

--- main.js
+++ main.js
@@ -2,10 +2,10 @@
 var canvaswidth = 800;
 var canvasheight = 600;
 var balx = 60;
-var baly = 60;
+var baly = 30;
 var balsize = 30;
-var xspeedorgineel = 1;
-var yspeedorgineel = 1;
+var xspeedorgineel = 10;
+var yspeedorgineel = 6;
 var xspeed = xspeedorgineel;
 var yspeed = yspeedorgineel;
 
@@ -16,26 +16,28 @@ function setup() {
 }
 
 function draw() {
-  bal();
   background(230);
+  bal();
 }
 
 function bal() {
   fill(66, 66, 66);
   ellipse(balx, baly, balsize, balsize);
-  if (balx + balsize > canvaswidth) {
+  if (balx + balsize / 2> canvaswidth) {
     xspeed = xspeedorgineel * -1;
   }
-  if (baly + balsize > canvasheight) {
+  if (baly + balsize / 2 > canvasheight) {
     yspeed = yspeedorgineel * -1;
   }
-  if (balx < 0) {
+  if (balx - balsize / 2 < 0) {
     xspeed = xspeedorgineel;
   }
-  if (baly < 0) {
+  if (baly - balsize / 2 < 0) {
     yspeed = yspeedorgineel;
   }
 
   balx = balx + xspeed;
   baly = baly + yspeed;
+  console.log(balx);
+  console.log(baly);
 }
