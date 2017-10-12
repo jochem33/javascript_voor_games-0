73b1bb905952e32bca805bfccec77245a8f3a4e9
jochem33 <jochemjuriaan.baas@coderclass.nl>
2017-10-12 18:06:33 +0200

    [change] bezig met code met this. begrijpen

--- main.js
+++ main.js
@@ -1,41 +1,50 @@
 //variabelen maken:
 var canvaswidth = 800;
 var canvasheight = 600;
-var balx = 60;
-var baly = 30;
-var balsize = 30;
+var bal1;
 var xspeedorgineel = 10;
 var yspeedorgineel = 6;
-var xspeed = xspeedorgineel;
-var yspeed = yspeedorgineel;
-
 
 function setup() {
   createCanvas(canvaswidth, canvasheight);
   background(230);
+  balletje1 = new balfunc(40, 40, 20, 5, 3);
+  balletje1.x = 40;
+  balletje1.y = 40;
+  balletje1.straal = 20;
+  balletje1.xspeed = xspeedorgineel;
+  balletje1.yspeed = yspeedorgineel;
 }
 
 function draw() {
-  background(230);
-  bal();
+  background(200);
+  balletje1.teken();
+  balletje1.beweeg();
 }
 
-function bal() {
-  fill(66, 66, 66);
-  ellipse(balx, baly, balsize, balsize);
-  if (balx + balsize / 2> canvaswidth) {
-    xspeed = xspeedorgineel * -1;
-  }
-  if (baly + balsize / 2 > canvasheight) {
-    yspeed = yspeedorgineel * -1;
-  }
-  if (balx - balsize / 2 < 0) {
-    xspeed = xspeedorgineel;
-  }
-  if (baly - balsize / 2 < 0) {
-    yspeed = yspeedorgineel;
+function balfunc(x, y, straal, xspeed, yspeed) {
+  this.x;
+  this.y;
+  this.straal;
+  this.xspeed;
+  this.yspeed;
+
+  this.teken = function() {
+    ellipse(this.x, this.y, this.straal, this.straal)
   }
 
-  balx = balx + xspeed;
-  baly = baly + yspeed;
+  this.beweeg = function() {
+    if (this.x + this.straal / 2> canvaswidth) {
+      xspeed = xspeedorgineel * -1;
+    }
+    if (this.y + this.straal / 2 > canvasheight) {
+      yspeed = yspeedorgineel * -1;
+    }
+    if (balx - balsize / 2 < 0) {
+      xspeed = xspeedorgineel;
+    }
+    if (baly - balsize / 2 < 0) {
+      yspeed = yspeedorgineel;
+    }
+  }
 }
