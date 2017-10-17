3f97fcd1df59d3dc98dad25712ca32c1061b225b
jochem33 <jochemjuriaan.baas@coderclass.nl>
2017-10-17 12:42:48 +0200

    []

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 //variabelen maken:
-var canvaswidth = 800;
-var canvasheight = 600;
+var canvaswidth = window.innerWidth - 30;
+var canvasheight = window.innerHeight - 30;
 var bal1;
-var xspeedorgineel = 5;
-var yspeedorgineel = 1;
+var xspeedorgineel;
+var yspeedorgineel;
 var ballen = [];
-var aantalballen = 50;
+var aantalballen = 1;
+var balstraal = 40;
 
 function setup() {
+  frameRate(25)
   createCanvas(canvaswidth, canvasheight);
   background(230);
   for (var i = 0; i < aantalballen; i++){
-    balletje1 = new Ball(20, 6, 10, i * xspeedorgineel, yspeedorgineel); //Maak een nieuwe instantie van Bal()
+    var cr = Math.floor(Math.random() * 255);
+    var cg = Math.floor(Math.random() * 255);
+    var cb = Math.floor(Math.random() * 255);
+    xspeedorgineel = Math.floor(Math.random() * 30 + 10);;
+    yspeedorgineel = Math.floor(Math.random() * 30 + 10);;
+    balletje1 = new Ball(20, 6, balstraal, xspeedorgineel, yspeedorgineel, cr, cg, cb); //Maak een nieuwe instantie van Bal()
     // balletje2 = new Ball(2, 21, 30, 2, 1); //Maak een nieuwe instantie van Bal()
 
     ballen.push(balletje1); //Push voegt de bal aan het einde van het array toe
@@ -19,8 +26,8 @@ function setup() {
 }
 
 function draw() {
-  background(200);
-
+  background(255, 255, 255, 10);
+  console.log(frameRate())
   for (var i = 0; i < ballen.length; i++){
     bal = ballen[i]; //Haal een bal uit het array
     bal.teken();
@@ -28,7 +35,7 @@ function draw() {
   }
 }
 
-function Ball(_x, _y, _straal, _xspeed, _yspeed) {
+function Ball(_x, _y, _straal, _xspeed, _yspeed, _cr, _cg, _cb) {
   this.x = _x;
   this.y = _y;
   this.straal = _straal;
@@ -36,10 +43,8 @@ function Ball(_x, _y, _straal, _xspeed, _yspeed) {
   this.yspeed = _yspeed;
 
   this.teken = function() {
-    this.cr = Math.floor(Math.random() * 255);
-    this.cg = Math.floor(Math.random() * 255);
-    this.cb = Math.floor(Math.random() * 255);
-    fill(this.cr, this.rg, this.rb);
+    noStroke();
+    fill(_cr, _cg, _cb);
     ellipse(this.x, this.y, this.straal, this.straal);
   }
 
